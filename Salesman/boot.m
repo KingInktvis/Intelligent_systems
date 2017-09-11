12705793dc3691aeafc2628f0631dea6ffea746c
KingInktvis <rikdevries253@hotmail.nl>
2017-09-11 13:55:04 +0200

    Calculate average, veriance and make a bad graph of temp samples

--- Salesman/boot.m
+++ Salesman/boot.m
@@ -1,10 +1,22 @@
 % City count
 n=100;
 % Amount of attempts to fix the path
-maxsteps=100;
+maxsteps=10;
 
-temp=1;
+temp=0.1;
 % The used method
 met=1;
 %Start app
-tsp(n,maxsteps,temp,met)
+% attempt1 = tsp(n,maxsteps,temp,met)
+
+x = [0.5 0.2 0.1 0.05 0.02 0.01];
+var = zeros(1, 6);
+ave = zeros(1, 6);
+
+for index = 1:6
+    temp = tsp(n,maxsteps,index,met);
+    ave(index) = temp(1);
+    var(index) = temp(2);
+end
+
+errorbar(x, ave, var)
--- Salesman/tsp.m
+++ Salesman/tsp.m
@@ -1,57 +1,64 @@
-   function anneal = tsp(n,maxsteps,temp,met)
-%  tsp(n,ms,temp,method) tries to find the shortest path 
+   function info = tsp(n,maxsteps,temp,met)
+%  tsp(n,ms,temp,method) tries to find the shortest path
 %  that connects n randomly placed cities
 %  method=1 (2) corresponds to Metropolis (threshold) algorithm
 %  ms*100 is the total number of performed steps
 %  temp is the initial temperature, after each 100 steps it
 %  is decreased by 1%.
-
+length = 0;
+variance=0;
    if (nargin<4)
       met =1;      % default: Metropolis algorithm
    end
-   if (nargin<3) 
+   if (nargin<3)
      temp = 0.1;   % default: T=0.1
    end
-   temps = temp;   % intial temperature 
+   temps = temp;   % intial temperature
 
-   lt = zeros(1,ceil(maxsteps));   
+   lt = zeros(1,ceil(maxsteps));
    tt = 1:ceil(maxsteps);
 
    close all;
-%  initialize random number generator and draw coordinates 
-   rand('state',0); cities = rand(n,2); 
+%  initialize random number generator and draw coordinates
+   rand('state',0); cities = rand(n,2);
    ord = [1:n];  op = path(ord,cities);
-  
+
    for jstep=1:ceil(maxsteps);
-%  lower temperature by 0.1 percent 
+%  lower temperature by 0.1 percent
    temp = temp*0.999;
-   for ins = 1:100 
+   for ins = 1:100
       j = ceil(rand*n); len = ceil(rand*(n/2));
       cand = reverse(ord,j,len);
-%  evaluate change of path length 
+%  evaluate change of path length
       diff = delta(ord,cities,j,j+len);
       np   = op + diff;
 %  met=1: threshold, met=2: metropolis
       if ( (met==1 && (rand<exp(-diff/temp))||(diff<0)) || ...
            (met==2 && diff<temp))
          ord = cand;
-         op = np; 
+         op = np;
       end
    end
+
 
 %  rescale length of path by sqrt(n) for output purposes
       lt(jstep) =  op/sqrt(n);
       curlen = path(ord,cities)/sqrt(n);
-%  plot map, cities and path 
+
+      length = length + curlen;
+      average = length / jstep;
+      variance = (variance * (jstep - 1) + ((curlen - average) ^ 2)) / jstep
+
+%  plot map, cities and path
       figure(1); plotcities(ord,cities);
       title(['n =',num2str(n,'%3.0f'),       ...
-             '   t =',num2str(jstep*100,'%8.0f'),  ... 
-             '   l =',num2str(curlen,'%4.4f'),  ... 
+             '   t =',num2str(jstep*100,'%8.0f'),  ...
+             '   l =',num2str(curlen,'%4.4f'),  ...
              '   T =',num2str(temp,'%6.6f')],   ...
              'fontsize',16);
-    if (met==1) 
+    if (met==1)
         xlabel(['Metropolis algorithm, annealing'],'fontsize',16);
-    else 
+    else
         xlabel(['Threshold algorithm', ...
                 '    T(0)=',num2str(temps,'%4.4f')], ...
                 'fontsize',16);
@@ -59,16 +66,16 @@
       pause(0.1);
    end
 %  plot evolution of length versus iteration step
-      figure(2); plot(0,0); hold on; 
-      plot(tt,lt,'k.'); 
+      figure(2); plot(0,0); hold on;
+      plot(tt,lt,'k.');
       title(['n =',num2str(n,'%3.0f'),       ...
-             '   l =',num2str(curlen,'%4.4f'),  ... 
-             '   T =',num2str(temps,'%4.4f')],   ... 
+             '   l =',num2str(curlen,'%4.4f'),  ...
+             '   T =',num2str(temps,'%4.4f')],   ...
              'fontsize',16);
-      if (met==1) 
+      if (met==1)
          xlabel(['Metropolis steps / 100'],'fontsize',16);
-      else 
+      else
          xlabel(['Threshold steps /100'],'fontsize',16);
       end
          ylabel(['l'],'fontsize',16);
-
+info = [average variance]
