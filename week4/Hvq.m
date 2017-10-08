9866e406cd4edb7e95c530dfd437b49564c37369
KingInktvis <rikdevries253@hotmail.nl>
2017-10-08 13:42:32 +0200

    Return value

--- week4/Hvq.m
+++ week4/Hvq.m
@@ -1,8 +1,9 @@
-function c = Hvq(prototypes, data)
+function d = Hvq(prototypes, data)
     distance = 0;
     s = size(data);
     for i=1:s(1)
         c = nearest(prototypes, data(i,:));
         distance = distance + distanceSquared(prototypes(c,:), data(i,:));
     end
+    d = distance;
 end
