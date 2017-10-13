08f4ceda3d960061885d59d4ea5ae458552cfe50
KingInktvis <rikdevries253@hotmail.nl>
2017-10-13 18:25:12 +0200

    .gitigore octave files

--- week5/assignment.m
+++ week5/assignment.m
@@ -1,4 +1,34 @@
 file = load('data_lvq.mat');
 data = file.w5_1;
-class1 = data(1:50, 1:2);
-class2 = data(51:100, 1:2);
+class0 = data(1:50, 1:2);
+class1 = data(51:100, 1:2);
+
+z = zeros(50, 1);
+o = ones(50, 1);
+third = [z; o];
+classes = [data, third];
+
+% Initilize parameters
+K = 2;
+n = 0.002;
+tMax = 10;
+
+% Initialize prototypes
+W = [];
+for c = 1 : 2
+    for  i = 1:K
+        x = rand();
+        y = rand();
+        Wi = [x y c - 1];
+        W = [W; Wi];
+    end
+end
+
+W2 = [];
+for i = 1:K
+    x = rand();
+    y = rand();
+    Wi = [x y];
+    W2 = [W2; Wi];
+end
+scatter(class0)
